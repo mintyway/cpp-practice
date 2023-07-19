a9bc685286f9a58500d83cd2a65c4f512a966947
CWingsC <lqlw1470@naver.com>
2023-07-19 21:24:44 +0900

    Update main.cpp

--- practice/main.cpp
+++ practice/main.cpp
@@ -1,32 +1,8 @@
 #include <iostream>
-#include <array>
-#include <cstring>
 #include <vector>
-
-#pragma pack(push, 1)
-
-struct TestStruct
-{
-	int j;
-	short i;
-};
-
+#include <string>
 
 int main()
 {
-	TestStruct testStruct;
-	TestStruct deserialiedData;
-	const int StructSize = sizeof(testStruct);
-	std::array<std::byte, StructSize> serializedData;
-
-	testStruct.i = 9;
-	testStruct.j = 20;
-
-	std::memcpy(serializedData.data(), &testStruct, StructSize);
-
-	std::memcpy(&deserialiedData, serializedData.data(), serializedData.size());
-
-	std::cout << "branchTest" << std::endl;
-
 	return 0;
 }
