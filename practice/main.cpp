fe038681318a758739a5eb1f7284f703a9583545
mintyway0112 <mintyway0112@gmail.com>
2023-10-20 06:10:37 +0900

    테스트

--- practice/main.cpp
+++ practice/main.cpp
@@ -6,9 +6,15 @@ float Multiply(const float LeftNumber, const float RightNumber)
 	return Result;
 }
 
+void TestPrint()
+{
+	std::cout << "테스트입니다." << std::endl;
+}
+
 int main()
 {
 	const float MultiplyResult = Multiply(3, 5);
 	std::cout << MultiplyResult << std::endl;
+	TestPrint();
 	return 0;
 }
